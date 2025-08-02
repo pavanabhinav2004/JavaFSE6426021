500444a1d294db3883b4f09b3f083f5069d747ab
pavanabhinav2004 <pavancharan935@gmail.com>
2025-08-02 17:02:29 +0530

    Add files via upload

---
+++ Java FSE DeepSkilling/Algorithms and Data Structures/Exercise 3/Order.java
@@ -0,0 +1,11 @@
+public class Order {
+    int orderId;
+    String customerName;
+    double totalPrice;
+
+    public Order(int orderId, String customerName, double totalPrice) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.totalPrice = totalPrice;
+    }
+}
---
+++ Java FSE DeepSkilling/Algorithms and Data Structures/Exercise 4/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+    int employeeId;
+    String name;
+    String position;
+    double salary;
+
+    public Employee(int employeeId, String name, String position, double salary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+}
